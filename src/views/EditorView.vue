57180c8bc2525c0082673888b71ca681082f9b6e
beamedeiros <beatriz.santos@prima.com.br>
2024-03-20 23:52:13 -0300

    adicionar evento

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
 import Quill from 'quill'
 import Navbar from '@/components/Navbar.vue'
-import { onMounted, ref } from 'vue'
+import { ElNotification } from 'element-plus'
+import { onMounted, ref, reactive, h } from 'vue'
+import api from '../service/api'
+import { useRouter } from 'vue-router'
 
 let quill: any
 const editorContent = ref<string>('')
+const router = useRouter()
+const token = ref(localStorage.getItem('token'))
+const form = reactive({
+  name: '',
+  dateInit: '',
+  timeInit: '',
+  dateFinish: '',
+  timeFinish: '',
+  content: editorContent.value,
+  value: null,
+  max_person: null
+})
 
 onMounted(() => {
   quill = new Quill('#editor', {
@@ -20,67 +35,187 @@ onMounted(() => {
     editorContent.value = quill.root.innerHTML
   })
 })
+
+const success = () => {
+  ElNotification({
+    title: 'Sucesso',
+    message: 'Evento cadastrado com sucesso!',
+    type: 'success',
+    duration: 2500
+  })
+}
+
+const fail = () => {
+  ElNotification({
+    title: 'Erro',
+    message: 'Erro ao cadastrar evento!',
+    type: 'error'
+  })
+}
+
+function onSubmit() {
+  api
+    .post(
+      '/event',
+      {
+        name: form.name,
+        init: `${form.dateInit}T${form.timeInit}:00`,
+        finish: `${form.dateFinish}T${form.timeFinish}:00`,
+        content: editorContent.value,
+        value: Number(`${form.value}.00`),
+        max_person: form.max_person
+      },
+      {
+        headers: { Authorization: `Bearer ${token.value}` }
+      }
+    )
+    .then((res) => {
+      success()
+      console.log(res)
+      router.push('/events')
+    })
+    .catch((err) => {
+      fail()
+      console.log(err)
+    })
+}
 </script>
 <template>
   <div>
     <navbar />
   </div>
-  <div class="editor-container">
-    <div id="toolbar-container">
-      <span class="ql-formats">
-        <select class="ql-font"></select>
-        <select class="ql-size"></select>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-bold"></button>
-        <button class="ql-italic"></button>
-        <button class="ql-underline"></button>
-        <button class="ql-strike"></button>
-      </span>
-      <span class="ql-formats">
-        <select class="ql-color"></select>
-        <select class="ql-background"></select>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-script" value="sub"></button>
-        <button class="ql-script" value="super"></button>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-header" value="1"></button>
-        <button class="ql-header" value="2"></button>
-        <button class="ql-blockquote"></button>
-        <button class="ql-code-block"></button>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-list" value="ordered"></button>
-        <button class="ql-list" value="bullet"></button>
-        <button class="ql-indent" value="-1"></button>
-        <button class="ql-indent" value="+1"></button>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-direction" value="rtl"></button>
-        <select class="ql-align"></select>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-link"></button>
-        <button class="ql-image"></button>
-        <button class="ql-video"></button>
-        <button class="ql-formula"></button>
-      </span>
-      <span class="ql-formats">
-        <button class="ql-clean"></button>
-      </span>
+  <div class="details-container">
+    <h1>Detalhes do evento</h1>
+    <div class="form-container">
+      <el-form :model="form" label-width="auto" :label-position="'left'">
+        <el-form-item label="Nome do evento">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="Data Início">
+          <el-col :span="11">
+            <el-date-picker
+              v-model="form.dateInit"
+              format="DD/MM/YYYY"
+              value-format="YYYY-MM-DD"
+              type="date"
+            />
+          </el-col>
+          <el-col :span="2" class="text-center">
+            <span class="text-gray-500">-</span>
+          </el-col>
+          <el-col :span="11">
+            <el-time-select
+              v-model="form.timeInit"
+              style="width: 240px"
+              start="01:00"
+              step="00:05"
+              end="24:00"
+              placeholder=""
+            />
+          </el-col>
+        </el-form-item>
+        <el-form-item label="Data Fim">
+          <el-col :span="11">
+            <el-date-picker
+              v-model="form.dateFinish"
+              format="DD/MM/YYYY"
+              value-format="YYYY-MM-DD"
+              type="date"
+            />
+          </el-col>
+          <el-col :span="2" class="text-center">
+            <span class="text-gray-500">-</span>
+          </el-col>
+          <el-col :span="11">
+            <el-time-select
+              v-model="form.timeFinish"
+              style="width: 240px"
+              start="01:00"
+              step="00:05"
+              end="24:00"
+              placeholder=""
+            />
+          </el-col>
+        </el-form-item>
+        <el-form-item label="Valor">
+          <el-input v-model="form.value" style="width: 222px">
+            <template #prepend>R$</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="Valor máximo de pessoas">
+          <el-input-number v-model="form.max_person" style="width: 222px" />
+        </el-form-item>
+      </el-form>
     </div>
-    <div id="editor"></div>
+    <div class="editor-container">
+      <div id="toolbar-container">
+        <span class="ql-formats">
+          <select class="ql-font"></select>
+          <select class="ql-size"></select>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-bold"></button>
+          <button class="ql-italic"></button>
+          <button class="ql-underline"></button>
+          <button class="ql-strike"></button>
+        </span>
+        <span class="ql-formats">
+          <select class="ql-color"></select>
+          <select class="ql-background"></select>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-script" value="sub"></button>
+          <button class="ql-script" value="super"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-header" value="1"></button>
+          <button class="ql-header" value="2"></button>
+          <button class="ql-blockquote"></button>
+          <button class="ql-code-block"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-list" value="ordered"></button>
+          <button class="ql-list" value="bullet"></button>
+          <button class="ql-indent" value="-1"></button>
+          <button class="ql-indent" value="+1"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-direction" value="rtl"></button>
+          <select class="ql-align"></select>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-link"></button>
+          <button class="ql-image"></button>
+          <button class="ql-video"></button>
+          <button class="ql-formula"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-clean"></button>
+        </span>
+      </div>
+      <div id="editor"></div>
+    </div>
+    <el-button type="primary" @click="onSubmit()">Cadastrar</el-button>
   </div>
 </template>
 
 <style scoped lang="less">
-.editor-container {
-  padding: 16px 40px;
+.details-container {
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
 
-  #editor {
-    height: 80vh;
+  .form-container {
+    .el-form {
+      padding: 8px 42px;
+    }
+  }
+  .editor-container {
+    padding: 16px 40px;
+
+    #editor {
+      height: 80vh;
+    }
   }
 }
 </style>
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,79 +1,85 @@
 <script setup lang="ts">
-import {ref, onMounted} from 'vue'
-import Navbar from '@/components/Navbar.vue';
-import api from '@/service/api';
-import { RouterLink } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import Navbar from '@/components/Navbar.vue'
+import api from '@/service/api'
+import { useRouter } from 'vue-router'
+import { View } from '@element-plus/icons-vue'
 
 interface Reservation {
-    email:string
-    name:string
-    celphone:string
-    num:number
-    event_id:number
+  email: string
+  name: string
+  celphone: string
+  num: number
+  event_id: number
 }
 
 interface Event {
-    name:string
-    init:string
-    finish:string
-    content:string
-    value:number
-    max_person:number
-    id:number
-    reservation:Reservation[]
+  name: string
+  init: string
+  finish: string
+  content: string
+  value: number
+  max_person: number
+  id: number
+  reservation: Reservation[]
 }
 
 const evt = ref<Event[]>([])
+const router = useRouter()
 
-function formatDate(date:string):string{
-    const d = new Date(date);
-    const formattedDate = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
-    return formattedDate
+function formatDate(date: string): string {
+  const d = new Date(date)
+  const formattedDate = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`
+  return formattedDate
 }
 
-onMounted(()=>{
-    api.get('/event')
-    .then(res=>{
-        evt.value = res.data
-    })
+onMounted(() => {
+  api.get('/event').then((res) => {
+    evt.value = res.data
+  })
 })
-
 </script>
 <template>
-    <Navbar/>
-    <div class="content">
-        <h1>Eventos Gastromar</h1>
-        <el-scrollbar max-height="600px">
-            <el-card class="event-card" v-for="x in evt" :key="x.id">
-                <div class="event-content">
-                    <div>
-                        <h3>{{ x.name }}</h3>
-                        <h4>Início: {{ formatDate(x.init) }}</h4>
-                        <h4>Fim: {{ formatDate(x.finish) }}</h4>
-                    </div>
-                    <RouterLink :to="'/event/'+x.id">view event</RouterLink>
-                </div>
-            </el-card>
-        </el-scrollbar>
-    </div>
+  <Navbar />
+  <div class="content">
+    <h1>Eventos Gastromar</h1>
+    <el-button @click="() => router.push('/editor')">adicionar</el-button>
+    <el-scrollbar max-height="600px">
+      <el-card class="event-card" v-for="x in evt" :key="x.id">
+        <div class="event-content">
+          <div>
+            <h3>{{ x.name }}</h3>
+            <h4>Início: {{ formatDate(x.init) }}</h4>
+            <h4>Fim: {{ formatDate(x.finish) }}</h4>
+          </div>
+          <!-- <RouterLink :to="'/event/' + x.id"> -->
+          <el-button type="primary" :icon="View" @click="() => router.push('/event/' + x.id)"
+            >visualizar</el-button
+          >
+          <!-- </RouterLink -->
+        </div>
+      </el-card>
+    </el-scrollbar>
+  </div>
 </template>
 <style>
-
-.content{
-    max-width: 1300px;
-    margin: 0 auto;
-    padding: 2rem;
-    font-weight: normal;
+.content {
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 
-.event-card{
-    margin-bottom: 14px;
+.event-card {
+  margin-bottom: 14px;
 }
 
-.event-content{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+.event-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-
 </style>
